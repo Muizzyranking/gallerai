8a14766bce0fb43c48c5983dec7c75b30cf4c833
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-13 13:03:15 +0100

    feat: postgres sql setup

---
+++ app/db/psql.py
@@ -0,0 +1,57 @@
+import uuid
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime, create_engine
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+from app.config import settings
+
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
+class BaseModel(Base):
+    __abstract__ = True
+    id: Mapped[str] = mapped_column(
+        UUID(as_uuid=False), primary_key=True, default=generate_uuid
+    )
+
+
+def utcnow():
+    return datetime.now(timezone.utc)
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=utcnow
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=utcnow, onupdate=utcnow
+    )
+
+
+def get_db():
+    """
+    DB Dependency, yeilds DB
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
