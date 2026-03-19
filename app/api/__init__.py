30117b926f76b12f23e576ffcb33e9f549ca4e42
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-19 06:29:58 +0100

    feat: add all routers together

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from . import auth, event, photos
+from . import auth, downloads, event, faces, gallery, photos
 
 router = APIRouter()
 
@@ -10,4 +10,19 @@ router.include_router(
     photos.router,
     prefix="/events/{event_id}/photos",
     tags=["photos"],
+)
+router.include_router(
+    faces.router,
+    prefix="/events/{event_id}/faces",
+    tags=["faces"],
+)
+router.include_router(
+    gallery.router,
+    prefix="/events/{event_id}/gallery",
+    tags=["gallery"],
+)
+router.include_router(
+    downloads.router,
+    prefix="/events/{event_id}",
+    tags=["downloads"],
 )
