50110e17f4899ab3510672a00a606a2d99a3f562
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-03-30 00:09:47 +0100

    chore: all models for easy import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,10 @@
-from app.models.user import User
+from app.models.user import User  # noqa: I001
 from app.models.event import Event, EventMember, EventInvite
 from app.models.photo import Photo
 from app.models.gallery import UserEventGallery
+from app.models.face_embedding import FaceEmbedding
+from app.models.platform import PlatformSettings
+from app.models.tokens import RefreshToken, PasswordResetToken
 
 __all__ = [
     "User",
@@ -10,4 +13,8 @@ __all__ = [
     "EventInvite",
     "Photo",
     "UserEventGallery",
+    "FaceEmbedding",
+    "PlatformSettings",
+    "RefreshToken",
+    "PasswordResetToken",
 ]
